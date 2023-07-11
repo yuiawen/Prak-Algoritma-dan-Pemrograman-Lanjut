1514b2842ea5484ebd8fc03492488a86cd07868a
Ikhwan Alidae <126540391+yuiawen@users.noreply.github.com>
2023-07-11 10:50:46 +0700

    Delete p_08 directory

--- p_08/method1.java
+++
@@ -1,9 +0,0 @@
-public class method1 {
-    public static void cetak() {
-        System.out.println("STMIK AKAKOM");
-    }
-
-    public static void main(String args[]) {
-        cetak();
-    }
-}
