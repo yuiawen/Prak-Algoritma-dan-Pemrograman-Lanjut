6a762cf7376a174b16de4a7e4d8054e90b89bc01
Ikhwan Alidae <126540391+yuiawen@users.noreply.github.com>
2023-07-11 10:51:35 +0700

    Add files via upload

---
+++ P_08/method1.java
@@ -0,0 +1,9 @@
+public class method1 {
+    public static void cetak() {
+        System.out.println("STMIK AKAKOM");
+    }
+
+    public static void main(String args[]) {
+        cetak();
+    }
+}
